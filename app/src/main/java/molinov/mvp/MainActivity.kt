f48568ea1a5f0e3b3475b1e77942fbc4ae34a8f3
VMolinov <molinov@list.ru>
2021-09-08 18:14:17 +0300

    Done

--- app/src/main/java/molinov/mvp/MainActivity.kt
+++ app/src/main/java/molinov/mvp/MainActivity.kt
@@ -1,11 +1,39 @@
 package molinov.mvp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import molinov.mvp.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MainView {
+
+    private var _vb: ActivityMainBinding? = null
+    private val vb get() = _vb!!
+    private val presenter = MainPresenter(this)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _vb = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(vb.root)
+        presenter.initButtons()
+        vb.btnCounter1.setOnClickListener { presenter.firstCounterClick() }
+        vb.btnCounter2.setOnClickListener { presenter.secondCounterClick() }
+        vb.btnCounter3.setOnClickListener { presenter.firstThirdClick() }
+    }
+
+    override fun setFirstButtonText(text: String) {
+        vb.btnCounter1.text = text
+    }
+
+    override fun setSecondButtonText(text: String) {
+        vb.btnCounter2.text = text
+    }
+
+    override fun setThirdButtonText(text: String) {
+        vb.btnCounter3.text = text
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _vb = null
     }
 }
